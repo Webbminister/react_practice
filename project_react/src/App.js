ce59dfa82c0a304b9b6cc0e0482236f381788584
Bamigbade <webbminister25@gmail.com>
2023-08-11 17:07:26 +0100

    my first react practice

---
+++ project_react/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./Navbar";
+import Cards from "./Card";
+import Heading from "./Heading";
+
+
+
+function App(){
+    return(
+        <div>
+            <NavBar />
+            <Heading />
+            <Cards />
+        </div>
+    )
+}
+
+export default App;
